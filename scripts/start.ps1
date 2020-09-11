e43b52d8c00214ac1c00de48cae034e55f017106
Nikhil Girraj <khandeln@tcd.ie>
2020-09-11 04:16:52 +0100

    Updated script for starting and stopping miners.

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -3,7 +3,7 @@
 	[double]$HPF
 )
 {
-	Start-Process -FilePath ../Miner.exe -ArgumentList "--name $MinerId", "--hash-power-factor $HPF"
+	Start-Process -FilePath ../Miner.exe -ArgumentList "--name $MinerId", "--hash-power-factor $HPF" -WindowStyle Hidden
 }
 
 function Stop-AllAltPowMiners
@@ -14,9 +14,34 @@ function Stop-AllAltPowMiners
 
 	)
 	if ($All.IsPresent -eq $true) {
-		Stop-Process -Name "Miner.exe" -Force
+		Stop-Process -Name "Miner" -Force
 	}
 	else {
 		
 	}
+}
+
+function Start-Miners 
+{
+	Param (
+		[Parameter(Mandatory=$false, ParameterSetName='A')][int]$Count,
+		[Parameter(Mandatory=$false, ParameterSetName='A')][double]$HashPower,
+		[Parameter(Mandatory=$false, ParameterSetName='B')][double[]]$HashPowers
+	)
+    
+    if($PSCmdlet.ParameterSetName -eq 'A')
+    {
+        for($i = 0; $i -lt $Count; $i++)
+        {
+            Start-AltPowMiner -MinerId "M$($i.ToString().PadLeft(2, '0'))" -HPF $HashPower
+        }
+    }
+    else {
+        $i = 0;
+        foreach($factor in $HashPowers)
+        {
+            Start-AltPowMiner -MinerId "M$($i.ToString().PadLeft(2, '0'))" -HPF $factor
+            $i++;
+        }
+    }
 }
